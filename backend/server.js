0234f980321e369a87bbdb6757960f59ef90e016
nkmakau <noshmak@gmail.com>
2020-09-13 16:54:54 +0300

    add mongodb path to server.js

--- backend/server.js
+++ backend/server.js
@@ -7,21 +7,21 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb://mongo:27017/';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
 const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
-mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true  } )
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 let db = mongoose.connection;
 
 // Check Connection
-db.once('open', ()=>{
+db.once('open', () => {
     console.log('Database connected successfully')
 })
 
 // Check for DB Errors
-db.on('error', (error)=>{
+db.on('error', (error) => {
     console.log(error);
 })
 
@@ -32,7 +32,7 @@ const app = express()
 app.use(express.json())
 
 // 
-app.use(upload.array()); 
+app.use(upload.array());
 
 // Cors 
 app.use(cors());
@@ -43,6 +43,6 @@ app.use('/api/products', productRoute)
 // Define the PORT
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
